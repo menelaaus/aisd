f6eca892786dcd7b8430e5f98b3a60b9009b5119
Aizhan <zhusupova02a@gmail.com>
2023-03-07 11:35:35 +0700

    функция разбиения

--- PolyphaseSort/PolyphaseSort/PolyphaseSort.cpp
+++ PolyphaseSort/PolyphaseSort/PolyphaseSort.cpp
@@ -11,6 +11,11 @@ using std::fstream;
 using std::endl;
 using std::ios;
 
+enum ErrorCode
+{
+	errorOpenFile 
+};
+
 bool createFileWithRandomNumbers(const string& fileName, const int numbersCount, const int maxNumberValue = 100) {
 	ofstream file(fileName);
 	if (!file) throw errorOpenFile;
@@ -40,4 +45,48 @@ bool isFileContainsSortedArray(const string& fileName) {
 	if (fileSorted) cout << "File " << fileName << " was sorted" << endl;
 	else cout << "File " << fileName << " was not sorted" << endl;
 	return fileSorted;
+}
+
+int splittingFile(int* D, int n, fstream** files, ifstream* initialFile)
+{
+	int N = 0;
+	int* A = new int[n];
+	for (int i = 0; i < n; i++) A[i] = D[i] = 1;
+	A[n - 1] = D[n - 1] = 0;
+	int nextNumber;
+	*initialFile >> nextNumber;
+	while (*initialFile)
+	{
+		for (int i = 0; (i < n) && *initialFile && (D[i] != 0); i++)
+		{
+			while ((D[i] > 0) && (*initialFile))
+			{
+				int lastNumber;
+				do
+				{
+					*files[i] << " " << nextNumber;
+					lastNumber = nextNumber;
+					*initialFile >> nextNumber;
+				} while ((lastNumber <= nextNumber) && (*initialFile));
+
+				D[i]--;
+				*files[i] << " " << -1;
+			}
+		}
+		N++;
+		if (*initialFile)
+		{
+			int oldA0 = A[0];
+			for (int i = 0; i < (n - 1); i++)
+			{
+				D[i] = A[i + 1] - A[i] + oldA0;
+				A[i] = A[i + 1] + oldA0;
+			}
+		}
+	}
+	cout << "A: ";
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	delete[]A;
+	return N;
 }
